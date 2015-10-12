3c197773a14d2532417c47d679426bb7a8644613
Miguel Angel Fernandez <elmendalerenda@gmail.com>
2015-10-12 17:22:33 +0000

    red on fizz test

--- test_fizz_buzz.rb
+++ test_fizz_buzz.rb
@@ -6,6 +6,12 @@ class FizzBuzzTest < Minitest::Test
     assert_equal(1, fizz_buzz(1))
   end
 
+  def test_fizz_for_numbers_multiple_of_3
+    assert_equal('Fizz', fizz_buzz(3))
+    assert_equal('Fizz', fizz_buzz(6))
+    assert_equal('Fizz', fizz_buzz(9))
+  end
+
   def fizz_buzz(number)
     number
   end
