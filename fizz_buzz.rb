1f25a7c742a719adfefe42d567694191d2426a5c
Miguel Angel Fernandez <elmendalerenda@gmail.com>
2015-10-18 22:08:11 +0000

    results as numbers

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -1,28 +1,29 @@
 def fizz_buzz(number)
   rules = [Fizz, Buzz, Bang]
-  result = rules.reduce(''){ |acc, rule| acc + rule.new.apply(number) }
+  result = rules.reduce(0){ |acc, rule| acc + rule.new.apply(number) }
 
-  return result unless result.empty?
+
+  return result unless result.zero?
   number
 end
 
 class Fizz
   def apply(number)
-    return 'Fizz' if (number % 3).zero?
-    return ''
+    return 3 if (number % 3).zero?
+    return 0
   end
 end
 
 class Buzz
   def apply(number)
-    return 'Buzz' if (number % 5).zero?
-    return ''
+    return 5 if (number % 5).zero?
+    return 0
   end
 end
 
 class Bang
   def apply(number)
-    return 'Bang' if (number % 7).zero?
-    return ''
+    return 7 if (number % 7).zero?
+    return 0
   end
 end
--- test_fizz_buzz.rb
+++ test_fizz_buzz.rb
@@ -8,32 +8,32 @@ class FizzBuzzTest < Minitest::Test
   end
 
   def test_fizz_for_numbers_multiple_of_3
-    assert_equal('Fizz', fizz_buzz(3))
-    assert_equal('Fizz', fizz_buzz(6))
-    assert_equal('Fizz', fizz_buzz(9))
+    assert_equal(3, fizz_buzz(3))
+    assert_equal(3, fizz_buzz(6))
+    assert_equal(3, fizz_buzz(9))
   end
 
   def test_buzz_for_numbers_multiple_of_5
-    assert_equal('Buzz', fizz_buzz(5))
-    assert_equal('Buzz', fizz_buzz(10))
-    assert_equal('Buzz', fizz_buzz(20))
+    assert_equal(5, fizz_buzz(5))
+    assert_equal(5, fizz_buzz(10))
+    assert_equal(5, fizz_buzz(20))
   end
 
   def test_fizzbuzz_for_numbers_multiple_of_3_and_5
-    assert_equal('FizzBuzz', fizz_buzz(5*3))
-    assert_equal('FizzBuzz', fizz_buzz(5*3*2))
-    assert_equal('FizzBuzz', fizz_buzz(5*3*11))
+    assert_equal(8, fizz_buzz(5*3))
+    assert_equal(8, fizz_buzz(5*3*2))
+    assert_equal(8, fizz_buzz(5*3*11))
   end
 
   def test_bang_for_multiple_of_7
-    assert_equal('Bang', fizz_buzz(7))
-    assert_equal('Bang', fizz_buzz(7*2))
+    assert_equal(7, fizz_buzz(7))
+    assert_equal(7, fizz_buzz(7*2))
   end
 
   def test_fizzbang_for_numbers_multiple_of_3_and_7
-    assert_equal('FizzBang', fizz_buzz(3 * 7))
-    assert_equal('FizzBang', fizz_buzz(3 * 7 * 2))
-    assert_equal('FizzBang', fizz_buzz(3 * 7 * 11))
+    assert_equal(10, fizz_buzz(3 * 7))
+    assert_equal(10, fizz_buzz(3 * 7 * 2))
+    assert_equal(10, fizz_buzz(3 * 7 * 11))
   end
 end
 
